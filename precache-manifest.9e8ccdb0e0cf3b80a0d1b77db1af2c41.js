0f6ff816bf09de837de4663af770b31977a46aca
Fabio Carrella <fabiocarrella@gmail.com>
2020-07-20 20:34:37 +0200

    Updates

---
+++ precache-manifest.9e8ccdb0e0cf3b80a0d1b77db1af2c41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9bd9b264d450b91660cf40c0cf27ce",
+    "url": "/blog-admin/index.html"
+  },
+  {
+    "revision": "c8471b7d7887057bc40e",
+    "url": "/blog-admin/static/css/main.e6ab9573.chunk.css"
+  },
+  {
+    "revision": "593c0bdfccd402738acc",
+    "url": "/blog-admin/static/js/2.6b2d2a06.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/blog-admin/static/js/2.6b2d2a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8471b7d7887057bc40e",
+    "url": "/blog-admin/static/js/main.7e9162f2.chunk.js"
+  },
+  {
+    "revision": "370c8385945f82f57222",
+    "url": "/blog-admin/static/js/runtime-main.44383e09.js"
+  }
+]);
